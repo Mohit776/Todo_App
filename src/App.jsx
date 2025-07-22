e851c03a02b22897290169449b4baf47c285b462
Mohit776 <mohitaggarwal551@gmail.com>
2025-07-22 19:34:42 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 import Navbar from './Components/Navbar'
 import { v4 as uuidv4 } from 'uuid';
-
-
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom';
 import { useState, useEffect } from 'react'
+import About from './Components/About';
+
+
+  
+
+
 
 function App() {
   const [todo, settodo] = useState("")
@@ -17,41 +22,34 @@ function App() {
     }
   }, [])
 
-
-
-
   const saveToLs = () => {
     localStorage.setItem("todos", JSON.stringify(todos))
-
   }
-
-
 
   const handleAdd = (params) => {
     settodos([...todos, { id: uuidv4(), todo, iscompleted: false }])
     settodo("")
     saveToLs()
-
-
   }
+
   const changeHandle = (e) => {
     settodo(e.target.value)
-
   }
+
   const handleEdit = (e, id) => {
     let t = todos.filter(item => {
       return item.id === id
     });
+
     settodo(t[0].todo)
     let newtodo = todos.filter(item => {
       return item.id !== id
     });
+
     settodos(newtodo)
     saveToLs()
 
   }
-
-
 
   const handleDelete = (e, id) => {
 
@@ -61,7 +59,6 @@ function App() {
     settodos(newtodo)
     saveToLs()
   }
-
 
   const Handlecheckbox = (e) => {
     let id = e.target.name
@@ -74,18 +71,20 @@ function App() {
     newtodo[index].isCompleted = !newtodo[index].isCompleted;
     settodos(newtodo)
     saveToLs()
-
   }
+
+
 
 
   return (
     <>
 
-      <Navbar />
+<Navbar />
+
       <div className="w-1/2 container bg-violet-100 py-4 rounded-md mx-auto p-5">
         <div className='flex justify-between'>
           <input placeholder='ADD A TODO' type="text" id="" className='w-96 px-4' onChange={changeHandle} value={todo} />
-          <button className='add bg-blue-500  w-20 font-semibold rounded-md h-10 hover:font-bold' type="button" onClick={handleAdd}>Save</button>
+          <button className='add bg-blue-500  w-20 font-semibold rounded-md h-10 hover:font-bold hover:underline' type="button" onClick={handleAdd}>Save</button>
         </div>
         <div className='yourtodo'>
           <h1 className='font-semibold my-3'>Your Todos</h1>
@@ -103,18 +102,14 @@ function App() {
                   <button type="button" className='w-10 h-9 rounded-md bg-blue-500  hover:font-bold' onClick={(e) => handleEdit(e, item.id)}>Edit</button>
                   <button type="button" className='w-14 h-9 rounded-md bg-blue-500  hover:font-bold' onClick={(e) => handleDelete(e, item.id)}>Delete</button>
                 </div>
-
-
-
               </div>
             })}
           </div>
 
-
         </div>
 
-
       </div>
+
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import About from './About'
 
 const Navbar = () => {
   return (
     <>
    <div className='flex justify-between bg-blue-300 py-4 rounded-md my-2 mx-3 '>
     <div className="logo">
-        <span className='mx-8 font-bold text-xl'>M#TODO</span>
+        <span className='mx-8 font-bold text-xl'>Piyush's Todo List</span>
     </div>
     <ul className='flex gap-8 px-4'>
-        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-        <li className='cursor-pointer hover:font-bold transition-all'>About</li>
-        <li className='cursor-pointer hover:font-bold transition-all'>Contact</li>
+        <li className='cursor-pointer hover:font-bold transition-all hover:underline'>Home</li>
+       <a href='/About'> <li className='cursor-pointer hover:font-bold transition-all hover:underline'>About</li></a>
+        <li className='cursor-pointer hover:font-bold transition-all hover:underline'>Contact</li>
     </ul>
    </div>
     
